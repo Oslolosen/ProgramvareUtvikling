486674d68014e064f40fda01c1ccea281e09dc84
Oslolosen <97946230+Oslolosen@users.noreply.github.com>
2023-01-20 12:31:41 +0100

    Add files via upload

---
+++ C#/SomeGuy/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace SomeGuy;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Guy joe = new Guy("Joe", 10000);
+        Guy bob = new Guy("Bob", 10000);
+
+        while (true)
+        {
+            Console.WriteLine("Who wants to recive money?");
+
+        }
+    }
+}
