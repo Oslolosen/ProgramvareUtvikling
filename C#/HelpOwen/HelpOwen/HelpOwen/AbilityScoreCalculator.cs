e8e33a1bf917ade128e9288bbe4b96c773c69457
Oslolosen <Magnus0209@hotmail.com>
2023-01-27 13:32:55 +0100

    HelpOwen

---
+++ C#/HelpOwen/HelpOwen/HelpOwen/AbilityScoreCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpOwen
+{
+    class AbilityScoreCalculator
+    {
+        public int rollResults = 14;
+        public double divideBy = 1.75;
+        public int addAmount = 2;
+        public int minimum = 3;
+        public int score;
+
+        public void CalculateScore()
+        {
+            double divided = rollResults / divideBy;
+
+            int added = addAmount + (int)divided;
+
+            if(added < minimum)
+            {
+                score = minimum;
+            }
+            else
+            {
+                score = added;
+            }
+        }
+    }
+}
