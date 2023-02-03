0a3e45ba72c96dd84e4b5238d8cde587d6b9e831
Oslolosen <Magnus0209@hotmail.com>
2023-02-03 13:26:45 +0100

    Elephant

---
+++ C_Sharp/SwapElephants/SwapElephants/Elephant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace SwapElephants;
+
+public class Elephant
+{
+	public string name;
+	public int earSize;
+
+    public void whoAmI()
+    {
+        Console.WriteLine("\nMy name is: "+ name);
+        Console.WriteLine("My ears are " + earSize + " inches");
+    }
+    public void HearMessage(string message, Elephant whoSaidIt)
+    {
+        Console.WriteLine("\n"+name + " heard a message ");
+        Console.WriteLine(whoSaidIt.name + " said this: " + message);
+    }
+
+    public void speakTo(Elephant whoTalkTo, string message)
+    {
+        whoTalkTo.HearMessage(message, this);
+    }
+}
---
+++ C_Sharp/SwapElephants/SwapElephants/Program.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace SwapElephants;
+
+public class Program{
+       
+    public static void Main(string[] args)
+    {
+        Elephant lucida = new Elephant() { name = "Lucida", earSize = 33 };
+        Elephant lloyd = new Elephant() { name = "Lloyd", earSize = 40 };
+
+        while (true)
+        {
+            Console.WriteLine("\nPress 1 for " + lucida.name + " Press 2 for " + lloyd.name + " Press 3 for swap" + " Press 4 for speak");
+            char press = Console.ReadKey().KeyChar;
+        
+            if(press == '1')
+            {
+                lucida.whoAmI();
+            }
+            else if(press == '2')
+            {
+                lloyd.whoAmI();
+            }
+            else if(press == '3')
+            {
+                Elephant tmp;
+                tmp = lucida;
+                lucida = lloyd;
+                lloyd = tmp;
+                Console.WriteLine("\nElephants have been swaped");
+            }
+            else if(press == '4')
+            {
+                lucida.speakTo(lloyd, "Hey, Lloyd");
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid Choice");
+            }
+        }
+    }   
+}
