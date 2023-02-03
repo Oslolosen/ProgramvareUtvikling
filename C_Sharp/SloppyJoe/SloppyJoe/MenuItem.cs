d0e7de1211c4c03144edeaba417a384723499bed
Oslolosen <Magnus0209@hotmail.com>
2023-02-03 14:36:44 +0100

    SloppyJoe

---
+++ C_Sharp/SloppyJoe/SloppyJoe/MenuItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SloppyJoe
+{
+    class MenuItem
+    {
+        public Random random = new Random();
+        public string[] meat = { "Roast beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
+        public string[] sauce = { "yellow mustard", "brown mustard", "honey mustard", "mayo", "relish", "french dressing" };
+        public string[] bread = { "rye", "white", "wheat", "pumpernickel", "a roll" };
+
+        public string description = "";
+        public string price;
+
+
+        public void generate()
+        {
+            this.price = "";
+            this.description = "";
+
+
+            description += meat[random.Next(meat.Length)];
+            description += " "+sauce[random.Next(sauce.Length)];
+            description += " "+bread[random.Next(bread.Length)];
+
+            decimal price = random.Next(2, 5) + random.Next(1, 98);
+            this.price = price.ToString("C");
+        }
+
+        public string toString()
+        {
+            return "Todays menu: "+description+"\nPrice :" + price;
+        }
+    }
+}
---
+++ C_Sharp/SloppyJoe/SloppyJoe/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SloppyJoe;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        MenuItem menu = new();
+
+        for (int i = 0; i < 10; i++)
+        {
+            menu.generate();
+
+            Console.WriteLine(menu.toString());
+        }     
+    }
+}
